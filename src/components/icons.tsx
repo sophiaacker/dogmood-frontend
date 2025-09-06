bde55427700a8fc38b471c6ec6c33f64e6ddec57
Jacob <jacobnt03@gmail.com>
2025-09-06 14:25:26 +0000

    no keep the dog exactly how I had it but why does the image look like th

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -31,46 +31,31 @@ export function SnoutScoutIcon(props: SVGProps<SVGSVGElement>) {
       viewBox="0 0 100 100"
       fill="none"
     >
+      <circle cx="56" cy="50" r="30" fill="hsl(var(--primary))" />
       <path
-        fill="hsl(var(--primary))"
-        d="M86.4 46.9c0-19.6-13.6-35.5-30.4-35.5-16.8 0-30.4 15.9-30.4 35.5 0 16.5 9.7 30.5 23.3 34.6 2.4.7 4.9 1.1 7.1 1.1s4.7-.4 7.1-1.1c13.6-4.1 23.3-18.1 23.3-34.6z"
-      ></path>
-      <path
-        fill="hsl(var(--primary-foreground))"
         d="M25.6 42.1c0-11.2 9.1-20.3 20.3-20.3V46c0 11.2-9.1 20.3-20.3 20.3S5.3 57.2 5.3 46c0-11.2 9.1-20.3 20.3-20.3v-4.6c-13.7 0-24.9 11.2-24.9 24.9s11.2 24.9 24.9 24.9 24.9-11.2 24.9-24.9V42.1z"
-      ></path>
-      <path
-        fill="hsl(var(--primary-foreground))"
-        d="M74.4 21.8c11.2 0 20.3 9.1 20.3 20.3v4.1c0 13.7-11.2 24.9-24.9 24.9s-24.9-11.2-24.9-24.9V46c0-11.2 9.1-20.3 20.3-20.3v-4.6c-13.7 0-24.9 11.2-24.9 24.9s11.2 24.9 24.9 24.9 24.9-11.2 24.9-24.9V46c0-13.7-11.2-24.9-24.9-24.9v.7z"
-      ></path>
-      <path
-        fill="hsl(var(--card))"
-        d="M58.7 66c-4.8 0-8.7-3.9-8.7-8.7v-2.1c0-4.8 3.9-8.7 8.7-8.7h.3c4.8 0 8.7 3.9 8.7 8.7v2.1c0 4.8-3.9 8.7-8.7 8.7h-.3z"
-      ></path>
-      <path
-        fill="hsl(var(--card))"
-        d="M41.3 66c-4.8 0-8.7-3.9-8.7-8.7v-2.1c0-4.8 3.9-8.7 8.7-8.7h.3c4.8 0 8.7 3.9 8.7 8.7v2.1c0 4.8-3.9 8.7-8.7 8.7h-.3z"
-      ></path>
-      <path
         fill="hsl(var(--foreground))"
-        d="M64 40.5a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0z"
-      ></path>
+      />
       <path
+        d="M85.4 46c0-13.7-11.2-24.9-24.9-24.9v4.6c11.2 0 20.3 9.1 20.3 20.3v4.1c0 13.7-11.2 24.9-24.9 24.9s-24.9-11.2-24.9-24.9V46c0-11.2 9.1-20.3 20.3-20.3V21.1C47.2 21.1 36 32.3 36 46v4.1c0 13.7 11.2 24.9 24.9 24.9s24.9-11.2 24.9-24.9V46z"
+        fill="hsl(var(--foreground))"
+      />
+      <path
+        d="M48.2,32.2c-3.1-2.4-6.8-3.9-10.8-3.9"
         stroke="hsl(var(--foreground))"
         strokeWidth="3"
         strokeLinecap="round"
-        d="M66 33.5c1.5-2 4-3.5 7-3.5"
-      ></path>
+      />
       <path
-        fill="hsl(var(--foreground))"
-        d="M39.5 40.5a3.5 3.5 0 11-7 0 3.5 3.5 0 017 0z"
-      ></path>
-      <path
+        d="M68.2,32.2c3.1-2.4,6.8-3.9,10.8-3.9"
         stroke="hsl(var(--foreground))"
         strokeWidth="3"
         strokeLinecap="round"
-        d="M34 33.5c-1.5-2-4-3.5-7-3.5"
-      ></path>
+      />
+      <circle cx="49.5" cy="42.5" r="3.5" fill="hsl(var(--foreground))" />
+      <circle cx="69.5" cy="42.5" r="3.5" fill="hsl(var(--foreground))" />
+      <circle cx="50" cy="65" r="8.7" fill="hsl(var(--card))" />
+      <circle cx="68" cy="65" r="8.7" fill="hsl(var(--card))" />
     </svg>
   );
 }
